fdc796ad293ed535912dd715c582bae5d595e5f3
AayushPokharel <official@aayushpokharel.com>
2022-12-20 09:59:01 +0545

    change pushed

--- Lab_Project/GameState.py
+++ Lab_Project/GameState.py
@@ -1,100 +1,88 @@
 class GameState:
-
     def __init__(self):
 
+        # fmt:off
         self.ValMap = [
-            0,0,0,
-            0,0,0,
-            0,0,0
-        ]
-        
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0
+            ]
+        # fmt: on
 
-        #self.player = ["playerX","playerO"]
+        # self.player = ["playerX","playerO"]
         self.currentPlayer = "playerX"
         self.won = False
         self.winningPlayer = ""
 
-    """
-    def changePlayer(self):
-        if self.currentPlayer == "playerX":
-            self.currentPlayer = "playerO"
-        if self.currentPlayer == "playerO":
-            self.currentPlayer = "playerX"
-    """
-    
     def getCurrentPlayer(self):
         return self.currentPlayer
 
-    def putValue(self,positionBox):
+    def putValue(self, positionBox):
         if self.ValMap[positionBox] == 0:
-            #print("putValue triggered")
+            # print("putValue triggered")
             if self.currentPlayer == "playerX":
                 self.ValMap[positionBox] = 1
                 self.currentPlayer = "playerO"
                 self.checkWin()
             elif self.currentPlayer == "playerO":
                 self.ValMap[positionBox] = 2
-                self.currentPlayer = "playerX"  
+                self.currentPlayer = "playerX"
                 self.checkWin()
-               
-        
-    
-    def checkIfSameValue(self,val1,val2,val3):
-        values = [self.ValMap[val1],self.ValMap[val2],self.ValMap[val3]]
-        if all(v==1 for v in values):
-            #print("1 win triggered")
+
+    def checkIfSameValue(self, val1, val2, val3):
+        values = [self.ValMap[val1], self.ValMap[val2], self.ValMap[val3]]
+        if all(v == 1 for v in values):
+            # print("1 win triggered")
             return "X Wins"
-        if all(v==2 for v in values):
-            #print("2 win triggered")
+        if all(v == 2 for v in values):
+            # print("2 win triggered")
             return "O Wins"
 
-    
     def checkHorizontal(self):
-        row1Status = self.checkIfSameValue(0,1,2)
-        row2Status = self.checkIfSameValue(3,4,5)
-        row3Status = self.checkIfSameValue(6,7,8)
+        row1Status = self.checkIfSameValue(0, 1, 2)
+        row2Status = self.checkIfSameValue(3, 4, 5)
+        row3Status = self.checkIfSameValue(6, 7, 8)
 
-        horizontalStatus = [row1Status,row2Status,row3Status]
+        horizontalStatus = [row1Status, row2Status, row3Status]
 
-        if any(status=="X Wins" for status in horizontalStatus):
+        if any(status == "X Wins" for status in horizontalStatus):
             self.won = True
             self.winningPlayer = "playerX"
 
-        if any(status=="O Wins" for status in horizontalStatus):
+        if any(status == "O Wins" for status in horizontalStatus):
             self.won = True
             self.winningPlayer = "playerO"
-    
+
     def checkVertical(self):
-        column1Status = self.checkIfSameValue(0,3,6)
-        column2Status = self.checkIfSameValue(1,4,7)
-        column3Status = self.checkIfSameValue(2,5,8)
+        column1Status = self.checkIfSameValue(0, 3, 6)
+        column2Status = self.checkIfSameValue(1, 4, 7)
+        column3Status = self.checkIfSameValue(2, 5, 8)
 
-        verticalStatus = [column1Status,column2Status,column3Status]
+        verticalStatus = [column1Status, column2Status, column3Status]
 
-        if any(status=="X Wins" for status in verticalStatus):
+        if any(status == "X Wins" for status in verticalStatus):
             self.won = True
             self.winningPlayer = "playerX"
 
-        if any(status=="O Wins" for status in verticalStatus):
+        if any(status == "O Wins" for status in verticalStatus):
             self.won = True
             self.winningPlayer = "playerY"
 
     def checkDiagonal(self):
-        diagonal1Status = self.checkIfSameValue(0,3,6)
-        diagonal2Status = self.checkIfSameValue(1,4,7)
+        diagonal1Status = self.checkIfSameValue(0, 3, 6)
+        diagonal2Status = self.checkIfSameValue(1, 4, 7)
 
-        diagonalStatus = [diagonal1Status,diagonal2Status]
+        diagonalStatus = [diagonal1Status, diagonal2Status]
 
-        if any(status=="X Wins" for status in diagonalStatus):
+        if any(status == "X Wins" for status in diagonalStatus):
             self.won = True
             self.winningPlayer = "playerX"
 
-        if any(status=="Y Wins" for status in diagonalStatus):
+        if any(status == "Y Wins" for status in diagonalStatus):
             self.won = True
             self.winningPlayer = "playerY"
 
-
-    def checkLoc(self,xLoc,yLoc):
+    def checkLoc(self, xLoc, yLoc):
         if xLoc >= 0 and xLoc <= 266 and yLoc >= 0 and yLoc <= 266:
             return 0
         elif xLoc >= 266 and xLoc <= 534 and yLoc >= 0 and yLoc <= 266:
@@ -107,7 +95,7 @@ class GameState:
             return 4
         elif xLoc >= 534 and xLoc <= 800 and yLoc >= 266 and yLoc <= 534:
             return 5
-        elif xLoc >= 0 and xLoc <= 266 and yLoc >=534 and yLoc <= 800:
+        elif xLoc >= 0 and xLoc <= 266 and yLoc >= 534 and yLoc <= 800:
             return 6
         elif xLoc >= 266 and xLoc <= 534 and yLoc >= 534 and yLoc <= 800:
             return 7
@@ -115,19 +103,27 @@ class GameState:
             return 8
 
     def checkWin(self):
-        #print("checkWin Triggered")
+        # print("checkWin Triggered")
         self.checkDiagonal()
         self.checkHorizontal()
         self.checkVertical()
 
         return self.won
 
-    def printBoardCondition(self,con):
+    def printBoardCondition(self, con):
         print(
-            con,"\n",
-            self.ValMap[0],self.ValMap[1],self.ValMap[2],"\n",
-            self.ValMap[3],self.ValMap[4],self.ValMap[5],"\n",
-            self.ValMap[6],self.ValMap[7],self.ValMap[8],"\n"
+            con,
+            "\n",
+            self.ValMap[0],
+            self.ValMap[1],
+            self.ValMap[2],
+            "\n",
+            self.ValMap[3],
+            self.ValMap[4],
+            self.ValMap[5],
+            "\n",
+            self.ValMap[6],
+            self.ValMap[7],
+            self.ValMap[8],
+            "\n",
         )
-
-    
--- Lab_Project/main.py
+++ Lab_Project/main.py
@@ -12,7 +12,7 @@ class App:
         # initialize python window
         pg.init()
         pg.display.set_caption("Tick Tack Toe | Lab Mini-Project")
-        pg.display.set_mode((800, 800), pg.OPENGL | pg.DOUBLEBUF)
+        self.scrn = pg.display.set_mode((800, 800), pg.OPENGL | pg.DOUBLEBUF)
         self.clock = pg.time.Clock()
         # initialize Game State
         self.gs = GameState()
@@ -21,6 +21,8 @@ class App:
         self.shader = self.createShader("shaders/vertex.txt", "shaders/fragment.txt")
         glUseProgram(self.shader)
         self.BackgroundLines = BackgroundLines()
+        self.imgX = pg.image.load("Texture/X.png").convert()
+        self.imgO = pg.image.load("Texture/O.png").convert()
         self.mainLoop()
 
     def createShader(self, vertexFilePath, fragmentFilePath):
@@ -47,16 +49,27 @@ class App:
                     running = False
 
                 if event.type == pg.MOUSEBUTTONDOWN:
-                    mouseX,mouseY = pg.mouse.get_pos()
-                    print("\nMouse Position Pixel :",mouseX,mouseY,"\n")
-                    #print(self.gs.checkLoc(int(mouseX),int(mouseY)))
-                    tileLocation = self.gs.checkLoc(int(mouseX),int(mouseY))
-                    print("value to be added in tile:",tileLocation)
-                    #print("before:\n",self.gs.ValMap,"\n\n")
+                    mouseX, mouseY = pg.mouse.get_pos()
+                    print("\nMouse Position Pixel :", mouseX, mouseY, "\n")
+                    # print(self.gs.checkLoc(int(mouseX),int(mouseY)))
+                    tileLocation = self.gs.checkLoc(int(mouseX), int(mouseY))
+                    print("value to be added in tile:", tileLocation)
+                    # print("before:\n",self.gs.ValMap,"\n\n")
                     self.gs.printBoardCondition("before")
+                    prevVal = self.gs.ValMap[tileLocation]
                     self.gs.putValue(tileLocation)
-                    #print("after:\n",self.gs.ValMap,"\n\n")
+                    curVal = self.gs.ValMap[tileLocation]
+                    # print("after:\n",self.gs.ValMap,"\n\n")
                     self.gs.printBoardCondition("after")
+                    if prevVal != curVal:
+                        print("change")
+                        newLine = [-0.5,0.5,0,1,1,1]
+                        self.BackgroundLines.addNew(newLine)
+                        print(self.BackgroundLines.vertices)
+                        glUseProgram(self.shader)
+                        glBindVertexArray(self.BackgroundLines.vao)
+                        glDrawArrays(GL_LINES, 0, self.BackgroundLines.vertex_count)
+
 
             # refresh screen
             glClear(GL_COLOR_BUFFER_BIT)
@@ -67,29 +80,27 @@ class App:
                 glDrawArrays(GL_LINES, 0, self.BackgroundLines.vertex_count)
             if self.gs.won == True:
                 glClearColor(1, 1, 1, 1)
-
+                #self.BackgroundLines.vertices.append()
 
             pg.display.flip()
 
             # timiing
             self.clock.tick(60)
-        
 
         self.quit()
-    
 
     def quit(self):
 
-        #self.flag.destroy()
+        # self.flag.destroy()
         glDeleteProgram(self.shader)
         pg.quit()
 
-class BackgroundLines:
 
+class BackgroundLines:
     def __init__(self):
-        #fmt: off
+        # fmt: off
         # x, y,z,r,g,b
-        self.vertices = (
+        self.raw_vertices = [
             -(1/3),1.0,0,1,1,1, 
             -(1/3),-1.0,0,1,1,1,
             (1/3),1.0,0,1,1,1,
@@ -98,37 +109,76 @@ class BackgroundLines:
             1.0, (1/3),0,1,1,1,
             -1.0, -(1/3),0,1,1,1,
             1.0, -(1/3),0,1,1,1,
-        )
-        self.vertices = np.array(self.vertices, dtype=np.float32)
+        ]
+        # fmt: on
+        self.vertices = np.array(self.raw_vertices, dtype=np.float32)
 
-        self.vertex_count = 8
+        #print("lenth of",len(self.vertices))
+        self.vertex_count = int(len(self.vertices)/6)
 
         self.vao = glGenVertexArrays(1)
         glBindVertexArray(self.vao)
         self.vbo = glGenBuffers(1)
         glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
         glBufferData(
-            GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices,GL_STATIC_DRAW
+            GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_DYNAMIC_DRAW
         )
         glEnableVertexAttribArray(0)
-        glVertexAttribPointer(0,3,GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
         glEnableVertexAttribArray(1)
-        glVertexAttribPointer(1,3,GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
 
+    def addNew(self,lis):
+        print("to be added",lis)
+        lis = np.array(lis,dtype=np.float32)
+        self.vertices = np.append(self.vertices,lis)
+        self.vertex_count = int(len(self.vertices)/6)
+        #self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        glBufferData(
+            GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_DYNAMIC_DRAW
+        )
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+    
     def destroy(self):
         glDeleteVertexArrays(1, (self.vao,))
         glDeleteBuffers(1, (self.vbo,))
+
+
+
 """
 class Material:
-
-    def __init__(self,filepath):
+    def __init__(self, filepath):
 
         self.texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.texture)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,GL_NEAREST)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+        image = pg.image.load(filepath).convert_alpha()
+        image_width, image_height = image.get_rect().size
+        image_data = pg.image.tostring(image, "RGBA")
+        glTexImage2D(
+            GL_TEXTURE_2D,
+            0,
+            GL_RGBA,
+            image_width,
+            image_height,
+            0,
+            GL_RGBA,
+            GL_UNSIGNED_BYTE,
+            image_data,
+        )
+        glGenerateMipmap(GL_TEXTURE_2D)
+
+    def use(self):
+        glActiveTexture(GL_TEXTURE0)
+        glBindTexture(GL_TEXTURE_2D, self.texture)
+
+    def destroy(self):
+        glDeleteTextures(1, (self.texture,))
 
 """
 
